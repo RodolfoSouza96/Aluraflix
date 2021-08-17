a115128031902850f8a8f1013a9a27d9a91a8274
Rodolfo <rodolfo.ls1996@outlook.com>
2021-08-17 16:11:05 -0300

    Colocando o RepositoryTest

---
+++ src/test/java/br/com/aluraflix/aluraflix/repository/VideoRepositoryTest.java
@@ -0,0 +1,14 @@
+package br.com.aluraflix.aluraflix.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VideoRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
